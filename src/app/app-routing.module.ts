684c8473add31ab079e65d39a8748b9544e1a726
franciscolopes <franciscolopesr26@gmail.com>
2021-04-19 10:58:58 +0100

    App files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./findtrip/findtrip.module').then(m => m.FindtripPageModule)
+    
+  },
+  {
+    path: 'findtrip2',
+    loadChildren: () => import('./findtrip2/findtrip2.module').then(m => m.FindtripPageModule)
+    
+  },
+  {
+    path: 'findtrip3',
+    loadChildren: () => import('./findtrip3/findtrip3.module').then(m => m.FindtripPageModule)
+    
+  },
+  {
+    path: 'findtrip4',
+    loadChildren: () => import('./findtrip4/findtrip4.module').then(m => m.FindtripPageModule)
+    
+  },
+  // { path: 'findtrip2', loadChildren: './findtrip2/findtrip2.module#FindtripPageModule2' },
+
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/findtrip/findtrip.page.ts
@@ -0,0 +1,115 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { NavController, AlertController } from '@ionic/angular';
+
+import { getLocaleDateFormat } from '@angular/common';
+import { map } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-findtrip',
+  templateUrl: './findtrip.page.html',
+  styleUrls: ['./findtrip.page.scss', './theme.css','./bootstrap.min.css','./js_composer.css'],
+
+})
+export class FindtripPage implements OnInit {
+  
+ 
+  rideoffers: Observable<any>;
+  isDriver: boolean
+
+  // constructor(
+  // )
+    // ) { 
+    //   this.rideofferCollection = afstore.collection<Rideoffer>('rideoffers');
+    //   this.rideoffers = this.afstore.collection('rideoffers').snapshotChanges().pipe(
+    //     map(actions => {
+    //       return actions.map(a => {
+    //         const data = a.payload.doc.data();
+    //         const id = a.payload.doc.id;
+    //         return { id, ...data };
+    //       });
+    //     })
+    //   );
+    //   firebase.auth().onAuthStateChanged(user =>{
+    //     if(user){
+    //       firebase
+    //     .firestore()
+    //     .doc(`users/${user.uid}`)
+    //     .get()
+    //     .then(usersSnapshot => {
+    //       this.isDriver = usersSnapshot.data().isDriver;
+    //       });
+    //     }
+    //   });
+    //   // this.mainuser = afstore.doc(`rideoffers/${user.getUID()}`)
+		// 	// this.sub = this.mainuser.valueChanges().subscribe(event => {
+    //   // this.phoneno = event.phoneno 
+    //   // console.log(this.phoneno)
+    //   // var link = 'https://wa.me/'+ this.phoneno
+    //   // console.log(link)
+		// //})
+    
+  
+    // navigateToChat(value) {
+    //   window.open('https://wa.me/'+ value)
+    // }
+  
+    ngOnInit() {
+    }
+
+
+    // goToRides() {
+    //   return this.navc.navigateForward(['/rides']);
+    // }
+
+    // saveToMyRequest(value){
+    //   this.afstore.doc("users/"+this.user.getUID()+"/myRequest/"+value.id).set({
+    //     pickup:  value.pickup,
+    //     destination: value.destination,
+    //     date: value.date,
+    //     time : value.time,
+    //     passenger: value.passenger,
+    //     fee : value.fee,
+    //     userid : value.userid,
+    //     phoneno : value.phoneno,
+    //     name : value.name,
+    //     isApprove: false,
+    //   }).then(()=>{
+    //     this.afstore.collection('rideoffers').doc(value.id).collection('requestList').doc("" + this.afstore.createId()).set({
+    //       isApproved: false,
+    //       userid: this.user.getUID(),
+    //       username: this.user.getUsername()
+    //     }).then(()=>{
+    //       this.presentAlert("Saved to My Request","Wait for user approval")
+    //     })
+        
+    //   })
+    // }
+
+    // async presentAlert(title: string, content: string) {
+    //   const alert = await this.alertController.create({
+    //     header: title,
+    //     message: content,
+    //     buttons: ['OK']
+    //   })
+  
+    //   await alert.present()
+    // }
+
+    // const searchbar = document.querySelector('ion-searchbar');
+    // const items = Array.from(document.querySelector('ion-list').children);
+    
+    // searchbar.addEventListener('ionInput', handleInput);
+    // function handleInput(event) {
+    //   const query = event.target.value.toLowerCase();
+    //   requestAnimationFrame(() => {
+    //     items.forEach(item => {
+    //       const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
+    //       item.style.display = shouldShow ? 'block' : 'none';
+    //     });
+    //   });
+    // }
+
+}
