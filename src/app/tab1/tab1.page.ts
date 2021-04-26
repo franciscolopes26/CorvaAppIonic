361bf1e55b2cfaced9a1e3616e0de9ff9428279f
franciscolopes <franciscolopesr26@gmail.com>
2021-04-26 06:37:22 +0100

    fix principal page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Navigation } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
 
 }
